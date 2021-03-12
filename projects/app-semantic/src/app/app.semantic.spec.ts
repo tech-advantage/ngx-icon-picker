e7b1a1ae6ad40a5c7a5499bd62ae204ef3ff0c79
Gregory Cochin <greg.coch5978@gmail.com>
2021-03-12 14:31:30 +0100

    Bump to Angular 10 (#37)
    Co-authored-by: Gregory COCHIN <Gregory.COCHIN@tech-advantage.com>

--- projects/app-semantic/src/app/app.semantic.spec.ts
+++ projects/app-semantic/src/app/app.semantic.spec.ts
@@ -1,19 +1,19 @@
-import { TestBed, async } from '@angular/core/testing';
+import {TestBed, waitForAsync} from '@angular/core/testing';
 import { AppSemanticComponent } from './app.semantic.component';
 describe('AppSemantic', () => {
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppSemanticComponent
       ],
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
+  it('should create the app', waitForAsync(() => {
     const fixture = TestBed.createComponent(AppSemanticComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'Icon Picker Example'`, async(() => {
+  it(`should have as title 'Icon Picker Example'`, waitForAsync(() => {
     const fixture = TestBed.createComponent(AppSemanticComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('Icon Picker Example');
