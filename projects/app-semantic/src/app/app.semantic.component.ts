6f4036115d1bb5a7c02bf6567773624b01d086d9
Michael Marcuccio <michael@marcuccio.com>
2023-01-03 14:13:05 -0600

    revert base proj to angular 13 compat;

--- projects/app-semantic/src/app/app.semantic.component.ts
+++ projects/app-semantic/src/app/app.semantic.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {UntypedFormControl, UntypedFormGroup} from '@angular/forms';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,14 +7,14 @@ import {UntypedFormControl, UntypedFormGroup} from '@angular/forms';
   styleUrls: ['./app.semantic.component.less']
 })
 export class AppSemanticComponent implements OnInit {
-  myFormGroup: UntypedFormGroup;
-  iconCssFas = new UntypedFormControl();
+  myFormGroup: FormGroup;
+  iconCssFas = new FormControl();
   fallbackIconFas = 'fas fa-user';
-  iconCssMat = new UntypedFormControl();
+  iconCssMat = new FormControl();
   fallbackIconMat = 'aspect_ratio';
 
   ngOnInit(): void {
-    this.myFormGroup = new UntypedFormGroup({
+    this.myFormGroup = new FormGroup({
       iconCssFas: this.iconCssFas,
       iconCssMat: this.iconCssMat
     });
